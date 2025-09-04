147f86eb4344b31a7083649512d210a1db1990f1
Schu1tz <72890497+ghsonda@users.noreply.github.com>
2025-09-04 05:28:37 -0300

    script.js

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ L.marker([-26.249, -48.633]).addTo(map)
 
 L.marker([-26.228, -48.645]).addTo(map)
   .bindPopup("<b>Comunidade de Pescadores - Vila da Glória</b><br>Pesca de camarão e peixe.");
+
+let zoomInicial;
+
+// Se a tela for menor que 768px (tablets/celulares), usa zoom mais perto
+if (window.innerWidth < 768) {
+  zoomInicial = 14;  // para celular
+} else {
+  zoomInicial = 12;  // para PC
+}
+
+var map = L.map('map').setView([-26.243, -48.634], zoomInicial);
+
+// Tile layer...
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
